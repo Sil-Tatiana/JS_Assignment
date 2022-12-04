93a57ad9cb2eaf1c37c5b1545c253e35c9fb25d8
Silvia Neves <siltatiana.sn@gmail.com>
2022-12-04 22:50:52 +0000

    creates game logic

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function userPlay() {
+  let promptText = "Choose: Rock, Paper or Scissors";
+  let userInput = prompt(promptText).toLowerCase();
+  while (
+    userInput !== "rock" &&
+    userInput !== "paper" &&
+    userInput !== "scissors"
+  ) {
+    userInput = prompt("Incorrect option\n" + promptText).toLowerCase();
+  }
+  return userInput;
+}
+
 function computerPlay() {
   const randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
@@ -9,31 +22,54 @@ function computerPlay() {
   }
 }
 
-// console.log(computerPlay());
+let playerScore = 0;
+let computerScore = 0;
 
-function singleRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
+function playRound() {
+  const playerSelection = userPlay();
+  const computerSelection = computerPlay();
   if (playerSelection === computerSelection) {
     return "It's a Draw!";
   } else if (playerSelection == "rock") {
     if (computerSelection == "paper") {
+      computerScore++;
       return "You lose! Paper beats Rock";
     } else {
+      playerScore++;
       return "You Won!";
     }
   } else if (playerSelection == "scissors") {
     if (computerSelection == "rock") {
+      computerScore++;
       return "You lose! Rock beats Scissors";
     } else {
+      playerScore++;
       return "You Won!";
     }
   } else if (playerSelection == "paper") {
     if (computerSelection == "scissors") {
+      computerScore++;
       return "You lose! Scissors beats Paper";
     } else {
+      playerScore++;
       return "You Won!";
     }
   }
 }
 
-console.log(singleRound("scissors", "scissors"));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound());
+    computerScore;
+    playerScore;
+  }
+  if (computerScore > playerScore) {
+    return `Computer: ${computerScore} points\n Player: ${playerScore} points\n Sorry😿! You lost, better luck next time!`;
+  } else if (computerScore < playerScore) {
+    return `Computer: ${computerScore} points\n Player: ${playerScore} points\nHOORAY🥳! You Won!`;
+  } else {
+    return `Computer: ${computerScore} points\n Player: ${playerScore} points\n OMG😱! DRAW, let's TRY AGAIN`;
+  }
+}
+
+console.log(game());
